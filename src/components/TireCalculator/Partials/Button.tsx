8a0c908e7fdeae194abbe4453ea0262cba24d740
SkorczanFFF <skorek11@gmail.com>
2023-11-26 16:23:15 +0100

    fix: button bottom padding

--- src/components/TireCalculator/Partials/Button.tsx
+++ src/components/TireCalculator/Partials/Button.tsx
@@ -26,7 +26,7 @@ const CalculateButton: React.FC<CalculateButtonProps> = ({
 }) => {
   return (
     <button
-      className={`bg-[#ff0000] mt-4 text-white hover:cursor-pointer hover:brightness-125 duration-150 font-[500] p-1 text-xl rounded-sm ${
+      className={`bg-[#ff0000] mt-4 text-white hover:cursor-pointer hover:brightness-125 duration-150 font-[500] p-1 text-xl rounded-sm pb-2 ${
         disabled && "disabled:cursor-not-allowed disabled:hover:brightness-100"
       }`}
       onClick={() => handleClick(width, profile, diameter, setResultCallback)}
